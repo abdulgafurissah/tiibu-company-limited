eaea189052a3771dff1a3a1585d1ea0ef1e690ec
Abdul Gafur Issah <abdulgafurissah@gmail.com>
2024-12-28 15:16:06 -0800

    another

--- src/admin-pages/admin.js
+++ src/admin-pages/admin.js
@@ -1,35 +1,10 @@
 import '../styles/admin.css';
+import AdminHeader from '../components/admin-view-header';
 function Admin() {
-
-    // JavaScript to toggle active section
-    const links = document.querySelectorAll('.sidebar button');
-    const sections = document.querySelectorAll('.main-content section');
-
-    links.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            links.forEach(l => l.classList.remove('active'));
-            link.classList.add('active');
-            sections.forEach(section => section.style.display = 'none');
-            document.querySelector(link.getAttribute('href')).style.display = 'block';
-        });
-    });
-
-    // Default display
-    sections.forEach((section, index) => section.style.display = index === 0 ? 'block' : 'none');
 
     return (
         <>
-            <div className="sidebar">
-                <h2>Admin Panel</h2>
-                <button className="active" data-link="#services">Manage Services</button>
-                <button data-link="#projects">Manage Projects</button>
-                <button data-link="#blog">Manage Blog Posts</button>
-                <button data-link="#messages">View Messages</button>
-                <button data-link="#jobs">Job Applications</button>
-                <button data-link="#logout">Logout</button>
-            </div>
-
+            <AdminHeader />
             <div className="main-content">
                 <section id="services">
                     <h1>Manage Services</h1>
